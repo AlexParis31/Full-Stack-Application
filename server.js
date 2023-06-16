35035297d8c1d9759a648a1b7db1531c8f6fdbff
Alexander Parisi <parisialexander@gmail.com>
2023-06-16 18:14:48 -0400

    install

--- server.js
+++ server.js
@@ -14,11 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 
-app.get('/', (req, res) => {
-    IconsSchema.find({}).then(()=> {
-        res.render('home.ejs');
-    })
-});
+
 
 
 
